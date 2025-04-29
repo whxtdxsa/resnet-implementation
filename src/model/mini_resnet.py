4c660a35c0478b9400f35e8000a6e40c69742360
root <whitdisa03@gmail.com>
2025-04-29 09:53:08 +0000

    t

--- src/model/mini_resnet.py
+++ src/model/mini_resnet.py
@@ -28,84 +28,7 @@ class ResidualBlock(nn.Module):
 
         return x
 
-
-class MiniResNet4(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self._build_layer(16, 32)
-
-        self.avepool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
-        self.fc1 = nn.Linear(32, 10)
-        self.relu = nn.ReLU()
-
-    def _build_layer(self, in_channels, out_channels):
-        layers = []
-        layers.append(ResidualBlock(in_channels, out_channels))
-        layers.append(ResidualBlock(out_channels, out_channels))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        
-        x = self.layer1(x)
-        
-        x = self.avepool(x)
-        
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
-        x = self.relu(x)
-
-        return x
-
-
-
-
-class MiniResNet6(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=7, stride=1, padding=3)
-        self.bn1 = nn.BatchNorm2d(8)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1) # 14
-
-        self.layer1 = self._build_layer(8, 16)
-
-        self.avepool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
-        self.fc1 = nn.Linear(16, 10)
-        self.relu = nn.ReLU()
-
-    def _build_layer(self, in_channels, out_channels):
-        layers = []
-        layers.append(ResidualBlock(in_channels, out_channels))
-        layers.append(ResidualBlock(out_channels, out_channels))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        
-        x = self.layer1(x)
-        
-        x = self.avepool(x)
-        
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
-        x = self.relu(x)
-
-        return x
-
-
-class MiniResNet(nn.Module): #best1
+class MiniResNet_v1(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=7, stride=2, padding=3)
@@ -141,3 +64,83 @@ class MiniResNet(nn.Module): #best1
 
         return x
 
+class MiniResNet_v2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1) # 14
+
+        self.layer1 = self._build_layer(16, 16)
+        self.layer2 = self._build_layer(16, 32)
+        self.layer3 = self._build_layer(32, 32)
+
+        self.avepool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.fc1 = nn.Linear(32, 10)
+        self.relu = nn.ReLU()
+
+    def _build_layer(self, in_channels, out_channels):
+        layers = []
+        layers.append(ResidualBlock(in_channels, out_channels))
+        layers.append(ResidualBlock(out_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        
+        x = self.avepool(x)
+        
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        return x
+
+class MiniResNet_v3(nn.Module):
+    # Sora
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1) # 14
+
+        self.layer1 = self._build_layer(32, 32)
+        self.layer2 = self._build_layer(32, 64)
+        self.layer3 = self._build_layer(64, 64)
+
+        self.avepool = nn.AdaptiveAvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.fc1 = nn.Linear(64, 10)
+        self.relu = nn.ReLU()
+
+    def _build_layer(self, in_channels, out_channels):
+        layers = []
+        layers.append(ResidualBlock(in_channels, out_channels))
+        layers.append(ResidualBlock(out_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        
+        x = self.avepool(x)
+        
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        return x
